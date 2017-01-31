97bd97113d7232d9a6ed0a7a56ef3e54b46099ab
Lars Winderling <lars.winderling@performance-advertising.de>
2017-01-31 21:27:48 +0100

    Add sequential test for ObjectPool.

---
+++ src/test/java/com/github/kahalemakai/objectpool/ObjectPoolTest.java
@@ -0,0 +1,71 @@
+package com.github.kahalemakai.objectpool;
+
+import lombok.val;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertEquals;
+
+public class ObjectPoolTest {
+    private ObjectPool<Integer> pool;
+    private final long expirationTime = 1000;
+    private AtomicInteger counter;
+
+    @Test
+    public void evict() throws Exception {
+        val borrowed = pool.borrow();
+        assertEquals(Integer.valueOf(1), Integer.valueOf(pool.elementsCreated()));
+        Thread.sleep(expirationTime);
+        pool.evictAll();
+        assertEquals(Integer.valueOf(1), Integer.valueOf(pool.elementsCreated()));
+        pool.unhand(borrowed);
+        pool.evictAll();
+        assertEquals(Integer.valueOf(1), Integer.valueOf(pool.elementsCreated()));
+        Thread.sleep(expirationTime);
+        pool.evictAll();
+        assertEquals(Integer.valueOf(0), Integer.valueOf(pool.elementsCreated()));
+        assertEquals(borrowed, pool.borrow());
+    }
+
+    @Test
+    public void sizes() throws Exception {
+        val list = new ArrayList<Integer>();
+        list.add(pool.borrow());
+        list.add(pool.borrow());
+        list.add(pool.borrow());
+        list.add(pool.borrow());
+        pool.unhand(list.get(3));
+        assertEquals(Integer.valueOf(4), Integer.valueOf(pool.elementsCreated()));
+        assertEquals(Integer.valueOf(4), Integer.valueOf(pool.elementsCreatedEstimate()));
+        assertEquals(Integer.valueOf(1), Integer.valueOf(pool.availableElements()));
+        assertEquals(Integer.valueOf(1), Integer.valueOf(pool.availableElementsEstimate()));
+    }
+
+    @Test
+    public void borrowing() throws Exception {
+        assertEquals(Integer.valueOf(0), pool.borrow());
+        assertEquals(Integer.valueOf(1), pool.borrow());
+        assertEquals(Integer.valueOf(2), pool.borrow());
+        val borrowed = pool.borrow();
+        assertEquals(Integer.valueOf(3), borrowed);
+        pool.unhand(borrowed);
+        assertEquals(Integer.valueOf(3), pool.borrow());
+    }
+
+    @Test
+    public void expirationTime() throws Exception {
+        assertEquals(expirationTime, pool.getExpirationTime());
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        counter = new AtomicInteger(0);
+        pool = new ObjectPool<>(
+                expirationTime,
+                counter::getAndIncrement,
+                (t) -> { counter.getAndDecrement(); });
+    }
+}
