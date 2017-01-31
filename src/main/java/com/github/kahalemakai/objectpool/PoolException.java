fdade668decc0d091920df1bdb7b3e91ac6f01e5
Lars Winderling <lars.winderling@performance-advertising.de>
2017-01-31 21:26:14 +0100

    Add very general exception PoolException.

---
+++ src/main/java/com/github/kahalemakai/objectpool/PoolException.java
@@ -0,0 +1,25 @@
+package com.github.kahalemakai.objectpool;
+
+/**
+ * The {@code PoolException} should be thrown
+ * whenever an interaction with a {@link Poolable}
+ * instance fails and no other more significant action
+ * is available, or if a checked exception needs to be
+ * wrapped.
+ */
+public class PoolException extends RuntimeException {
+    public PoolException() {
+    }
+
+    public PoolException(String message) {
+        super(message);
+    }
+
+    public PoolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PoolException(Throwable cause) {
+        super(cause);
+    }
+}
