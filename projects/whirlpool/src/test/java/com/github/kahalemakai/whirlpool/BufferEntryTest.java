2e80fdb91d4cadc52555f1615a16196bfc8e8478
Lars Winderling <lars.winderling@performance-advertising.de>
2017-02-28 16:12:15 +0100

    Test BufferEntry#get() and #set().

--- projects/whirlpool/src/test/java/com/github/kahalemakai/whirlpool/BufferEntryTest.java
+++ projects/whirlpool/src/test/java/com/github/kahalemakai/whirlpool/BufferEntryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.*;
 
@@ -52,6 +53,89 @@ public class BufferEntryTest {
         assertEquals(null, mTake.invoke(entry, requestId, bufferSize));
         assertTrue((boolean) mAdd.invoke(entry, requestId, newValue));
         assertEquals(null, mTake.invoke(entry, requestId+1, bufferSize));
+    }
+
+    @Test
+    public void get() throws Exception {
+        val sleepingTime = 5000;
+        val start = System.currentTimeMillis();
+        val requestId = 23L;
+        val newValue = 34234;
+        val bufferSize = 92342;
+        final BufferEntry<Integer> entry = BufferEntry.ofId(requestId);
+        val consumerSignal = new CountDownLatch(1);
+        val producerSignal = new CountDownLatch(1);
+        val consumer = new Thread(() -> {
+            try {
+                val v = entry.get(requestId, bufferSize);
+                assertEquals(newValue, (int) v);
+            } catch (InterruptedException e) {
+                throw new AssertionError("could not consume", e);
+            } finally {
+                consumerSignal.countDown();
+            }
+        });
+        val producer = new Thread(() -> {
+            try {
+                Thread.sleep(sleepingTime);
+                entry.set(requestId, newValue);
+            } catch (InterruptedException e) {
+                throw new AssertionError("could not produce", e);
+            } finally {
+                producerSignal.countDown();
+            }
+        });
+        assertNull(valueField.get(entry));
+        consumer.start();
+        producer.start();
+        consumerSignal.await();
+        val end = System.currentTimeMillis();
+        assertTrue((end - start) > sleepingTime);
+        assertNull(valueField.get(entry));
+        assertEquals(-(requestId + bufferSize), idField.get(entry));
+    }
+
+    @Test
+    public void set() throws Exception {
+        val sleepingTime = 5000;
+        val start = System.currentTimeMillis();
+        val requestId = 23L;
+        val newValue = 34234;
+        val finalValue = 11223344;
+        val bufferSize = 92342;
+        final BufferEntry<Integer> entry = BufferEntry.ofId(requestId);
+        val consumerSignal = new CountDownLatch(1);
+        val producerSignal = new CountDownLatch(1);
+        val consumer = new Thread(() -> {
+            try {
+                Thread.sleep(sleepingTime);
+                val v = entry.get(requestId, bufferSize);
+                assertEquals(newValue, (int) v);
+            } catch (InterruptedException e) {
+                throw new AssertionError("could not consume", e);
+            } finally {
+                consumerSignal.countDown();
+            }
+        });
+        val producer = new Thread(() -> {
+            try {
+                entry.set(requestId + bufferSize, finalValue);
+            } catch (InterruptedException e) {
+                throw new AssertionError("could not produce", e);
+            } finally {
+                producerSignal.countDown();
+            }
+        });
+        assertNull(valueField.get(entry));
+        assertTrue((boolean) mAdd.invoke(entry, requestId, newValue));
+        producer.start();
+        consumer.start();
+        producerSignal.await();
+        val end = System.currentTimeMillis();
+        assertTrue((end - start) > sleepingTime);
+        consumerSignal.await();
+        assertEquals(finalValue, valueField.get(entry));
+        assertEquals(requestId + bufferSize, idField.get(entry));
     }
 
     @Before
